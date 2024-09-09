67de0a2775a71b687b7e1fda7dbfea229f5195c1
peaceofsense <72244031+peaceofsense@users.noreply.github.com>
2024-09-09 14:58:38 +0200

    Home manager test

--- configuration.nix
+++ configuration.nix
@@ -108,7 +108,6 @@
   nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     blueman
     btop
--- flake.nix
+++ flake.nix
@@ -4,18 +4,28 @@
   
   inputs = {
     nixpkgsStable.url = "nixpkgs/nixos-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgsStable"; # looks for same version of packages
     nixpkgsUnstable.url = "nixpkgs/nixos-unstable";
   };
   
-  outputs = { self, nixpkgsStable, nixpkgsUnstable, ... } @ inputs:
+  outputs = { self, nixpkgsStable, nixpkgsUnstable, home-manager, ... } @ inputs:
     let
-      lib = nixpkgsStable.lib; # It is like pass nixpkgs to this file
+      lib = nixpkgsStable.lib; # It is like pass nixpkgs to this var
+      system = "x86_64-linux";
       #lib-un = inputs.nixpkgUnstable.lib;
+      pkgs = nixpkgsStable.legacyPackages.${system};
     in {
     nixosConfigurations = {
       nixos = lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [ ./configuration.nix ];
+        inherit system;        
+	modules = [ ./configuration.nix ];
+      };
+    };
+    homeConfigurations = {
+      peaceofsense = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+	modules = [ ./home.nix ]; 
       };
     };
   };
